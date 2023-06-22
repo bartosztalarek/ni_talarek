3fe86729f9b344d823e2a52b6883a8c4e12fad3a
Bartosz Talarek <bartosztalarek23@protonmail.com>
2023-06-22 17:23:31 +0200

    main - added method to display text

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello! How are you today?");
+﻿Display.DisplayText("Hello! How are you today?");
+
+public static class Display
+{
+    public static void DisplayText(string text)
+    {
+        Console.WriteLine(text);
+
+    }
+}
